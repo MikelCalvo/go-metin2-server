1fef4141fd72f459309c4b6f66e4440085d49fa2
Mikel Calvo <admin@mikelcalvo.net>
2026-07-07 00:16:30 +0200

    test: cover anti-stack item-move guard

--- internal/minimal/item_move_runtime_test.go
+++ internal/minimal/item_move_runtime_test.go
@@ -177,6 +177,7 @@ func TestGameRuntimeItemMoveRejectsTransferGuardedStackTemplatesWithoutMutation(
 		name   string
 		mutate func(*itemcatalog.Template)
 	}{
+		{name: "anti stack", mutate: func(template *itemcatalog.Template) { template.AntiStack = true }},
 		{name: "anti drop", mutate: func(template *itemcatalog.Template) { template.AntiDrop = true }},
 		{name: "anti give", mutate: func(template *itemcatalog.Template) { template.AntiGive = true }},
 		{name: "anti sell", mutate: func(template *itemcatalog.Template) { template.AntiSell = true }},
